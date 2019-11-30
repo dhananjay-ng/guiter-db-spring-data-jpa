161b0dd9307b4cf73e18da6b8e02b95a460fa2aa
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-11-30 20:53:05 +0530

    #Spring data Jpa Repositories added to replace existing normal Jpa Repositories

---
+++ src/main/java/com/guitar/db/repository/ManufacturerJpaRepository.java
@@ -0,0 +1,7 @@
+package com.guitar.db.repository;
+
+import com.guitar.db.model.Manufacturer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManufacturerJpaRepository extends JpaRepository<Manufacturer, Long> {
+}
